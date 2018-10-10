ccc56977a93fc4ba8c63c78b74a90a0b9e190597
franca5 <alexander.francoletti@gmail.com>
2018-10-10 16:09:40 -0400

    Added level details

---
+++ js/levelParser.js
@@ -0,0 +1,74 @@
+//Should just need a script to interpret level choice
+
+let levelParser = function(){
+	
+};
+
+levelParser.prototype.parse = function(Difficulty, Type){
+	if (Type === 1)	//Ground 
+	{
+
+	}
+	else if (Type === 2) //Ground going up
+	{
+
+	}
+	else if (Type === 3) //Building
+	{
+
+	}
+	else if (Type === 4) //Building going up
+	{
+
+	}
+	else if (Type === 5) //Building going down
+	{
+
+	}
+	else if (Type === 6) //Building going up and down
+	{
+
+	}
+	else if (Type === 7) //Building w gap
+	{
+
+	}
+	else if (Type === 8) //Ground w Powerup
+	{
+
+	}
+	else if (Type === 9) //Building w Powerup
+	{
+
+	}
+	else //Empty space
+	{
+
+	}
+
+	if (Difficulty === 1) //1 Hazard
+	{
+
+	}
+	else if (Difficulty === 2) //2 Hazards
+	{
+
+	}
+	else if (Difficulty === 3) //1 AI
+	{
+
+	}
+	else if (Difficulty === 4) //1 Hazard 1 AI
+	{
+
+	}
+	else if (Difficulty === 5) //Finish zone
+	{
+
+	}
+	else //No addons
+	{
+
+	}
+
+};
